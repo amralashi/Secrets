e3a34fe60dbb87ddfdb7d4803d93d58488a159e1
amralashi <eng.amralashi@gmail.com>
2023-02-08 00:29:15 -0500

    Passport | Cookies & Sessions

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const passport = require('passport');
+const passpotLocalMongoose = require("passport-local-mongoose");
 
-// var md5 = require('md5');
-const bcrypt = require('bcrypt');
-const saltRound = 10;
 
 mongoose.set('strictQuery', true);
 
@@ -19,6 +19,19 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
+
+app.use(session({
+    secret: "Our little secret.",
+    resave: false,
+    saveUninitialized: false
+
+}));
+
+//Passport initialization
+app.use(passport.initialize());
+//Telling passport to use session module
+app.use(passport.session());
+
 mongoose.connect("mongodb://127.0.0.1:27017/userDB", {useNewUrlParser: true});
 
 const userSchema = new mongoose.Schema({
@@ -26,48 +39,46 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-
+//Add the passpotLocalMongoose module as a plugin to the schema
+userSchema.plugin(passpotLocalMongoose);
 
 const userModel = mongoose.model('User', userSchema);
 
+//Establish a link between passport and the authentication fields in the schema
+passport.use(userModel.createStrategy()); 
+
+//Generates a function that is used by Passport to serialize users into the session
+passport.serializeUser(userModel.serializeUser());
+//Generates a function that is used by Passport to deserialize users into the session
+passport.deserializeUser(userModel.deserializeUser());
 //-------------------------------------------POST-------------------------------------------
 app.post("/register", (req, res)=>{
 
-    bcrypt.hash(req.body.password, saltRound, function(err, hash) {
-        const newUser = new userModel({
-            email: req.body.username,
-            password: hash
-        });
-
-        newUser.save((err)=>{
-            if(!err){
-                console.log('User has been added!');
-                res.render('secrets');
-            }else{
-                console.log(err);
-            }
-        });
+    userModel.register({username:req.body.username, active: false}, req.body.password, function(err, user) {
+        if (err) { 
+            console.log(err);
+            res.redirect("/register");
+        }else{
+            passport.authenticate('local')(req, res, function() {
+              res.redirect('/secrets');
+            });
+        }
     });
-
 });
 
 app.post("/login", (req, res)=>{
-    const username = req.body.username;
-    const password = req.body.password;
 
-    userModel.findOne({email: username}, (err, foundUser)=>{
-            if(foundUser){
-                bcrypt.compare(password, foundUser.password, function(err, result) { 
-                    if(result){
-                        res.render('secrets');
-                    }else{
-                        res.send("Wrong password!");
-                    }
-                });
-            }else{
-                res.send("This user is not registered!");
-            }
+    const user = new userModel({
+        username: req.body.username,
+        password: req.body.password
     });
+
+    //passport.authenticate() middleware invokes req.login() automatically.
+    //This function is primarily used when users sign up, during which req.login() can be invoked to automatically log in the newly registered user.
+    passport.authenticate('local', { failureRedirect: '/register' })(req, res, function() {
+        res.redirect('/secrets');
+    });
+
 });
 
 //-------------------------------------------GET-------------------------------------------
@@ -79,8 +90,25 @@ app.get("/login", (req, res)=>{
     res.render("login");
 });
 
+app.get("/logout", (req, res)=>{
+    req.logout((err)=>{
+        if(err){
+            console.log(err);
+        }
+        res.redirect("/");
+    });
+});
+
 app.get("/register", (req, res)=>{
     res.render("register");
+});
+
+app.get("/secrets", (req, res)=>{
+    if(req.isAuthenticated()){
+        res.render("secrets");
+    }else{
+        res.redirect("/");
+    }
 });
 
 
